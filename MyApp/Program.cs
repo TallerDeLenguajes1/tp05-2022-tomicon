d52e07925e4ce547d1885c73a260b35d4ec57ad6
tomicon <tomicon01@gmail.com>
2022-05-17 16:19:40 -0300

    commit de la rama2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello world!");
+Console.WriteLine("Ingrese un texto");
+string texto = Console.ReadLine();
+double numero= 4;
+Console.WriteLine("El texto ingresado es: " + texto + "\n y el numero es: " + numero);
