88abb51be31c240aafe317fb7672ad7f53c975c7
tomicon <tomicon01@gmail.com>
2022-05-30 15:49:08 -0300

    ejercicio 1 funcionando

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ingrese un texto");
-string texto = Console.ReadLine();
-double numero= 4;
-Console.WriteLine("El texto ingresado es: " + texto + "\n y el numero es: " + numero);
+Console.WriteLine("||Invertir un número entero||\n");
+int NumIngresado, NumInvertido;
+NumInvertido= 0;
+
+Console.WriteLine("Ingrese el numero que desea invertir (debe ser mayor que cero): ");
+NumIngresado= Convert.ToInt32(Console.ReadLine());
+if (NumIngresado <= 0)
+{
+    Console.WriteLine("No se realizara la inversion ya que el numero no es mayor que 0\n");
+}
+while (NumIngresado > 0)
+{
+    NumInvertido= NumInvertido * 10 + NumIngresado % 10;
+    NumIngresado= NumIngresado/10;
+}
+if (NumInvertido > 0)
+{
+    Console.WriteLine("El numero invertido es: " + NumInvertido);    
+}
+
+
