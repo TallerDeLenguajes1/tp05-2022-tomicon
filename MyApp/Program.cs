aedd7f2b25031bd7f8b30805822e882abfc3cb1e
tomicon <tomicon01@gmail.com>
2022-05-30 16:47:32 -0300

    Calculadora2 andando correctamente

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,45 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int operacion= 1;
-while (operacion != 5)
+Console.WriteLine("Ingrese un numero para realizarle diversas operaciones");
+double operando, comparado1, comparado2;
+operando= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El valor absoluto del numero ingresado es: " + Math.Abs(operando));
+if (operando < 0)
 {
-    Console.WriteLine("Que operacion desea reaizar? (1=suma, 2=resta, 3=multiplicacion, 4=division, 5=salir)\n");
-    operacion= Convert.ToInt32(Console.ReadLine());
-    double operando1, operando2, resultado;
-    switch (operacion)
-    {
-        case 1:
-            Console.WriteLine("Que numeros desea sumar?\n");
-            operando1= Convert.ToDouble(Console.ReadLine());
-            operando2= Convert.ToDouble(Console.ReadLine());
-            resultado= operando1 + operando2;
-            Console.WriteLine("El resultado es: " + resultado);
-            break;
-        case 2:
-            Console.WriteLine("Que numeros desea restar?\n");
-            operando1= Convert.ToDouble(Console.ReadLine());
-            operando2= Convert.ToDouble(Console.ReadLine());
-            resultado= operando1 - operando2;
-            Console.WriteLine("El resultado es: " + resultado);
-            break;
-        case 3:
-            Console.WriteLine("Que numeros desea multiplicar?\n");
-            operando1= Convert.ToDouble(Console.ReadLine());
-            operando2= Convert.ToDouble(Console.ReadLine());
-            resultado= operando1 * operando2;
-            Console.WriteLine("El resultado es: " + resultado);
-            break;
-        case 4:
-            Console.WriteLine("Que numeros desea dividir?\n");
-            operando1= Convert.ToDouble(Console.ReadLine());
-            operando2= Convert.ToDouble(Console.ReadLine());
-            resultado= operando1/operando2;
-            Console.WriteLine("El resultado es: " + resultado);
-            break;
-        case 5:
-            Console.WriteLine("Fin de la calculadora\n");
-            break;
-        default:
-            Console.WriteLine("La operacion ingresada no es valida\n");
-            break;
-    }
+    Console.WriteLine("la raiz cuadrada de el numero ingresado no esta definida");
+} else
+{
+    Console.WriteLine("La raiz cuadrada del numero ingresado es: " + Math.Sqrt(operando));   
 }
+Console.WriteLine("El seno del numero ingresado es: " + Math.Sin(operando));
+Console.WriteLine("El coseno del numero ingresado es: " + Math.Cos(operando));
+Console.WriteLine("La parte entera del numero ingresado es: " + Math.Truncate(operando));
+Console.WriteLine("Ingrese dos numeros para ser comparados");
+comparado1= Convert.ToDouble(Console.ReadLine());
+comparado2= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El maximo entre los dos numeros ingresados es: " + Math.Max(comparado1, comparado2));
+Console.WriteLine("El minimo entre los dos numeros ingresados es: " + Math.Min(comparado1, comparado2));
+
+
+
